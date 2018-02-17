867de066bfeffae6dfe4e3c65a966a5d8f0f35d3
jamie <ogelacinyc@gmail.com>
2018-02-17 20:56:34 +0900

    add log

--- service-worker.js
+++ service-worker.js
@@ -38,6 +38,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
+      console.log(filesToCache);
       return cache.addAll(filesToCache);
     })
   );
@@ -50,7 +51,6 @@ self.addEventListener('activate', function(e) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
-          console.log(filesToCache);
           return caches.delete(key);
         }
       }));
